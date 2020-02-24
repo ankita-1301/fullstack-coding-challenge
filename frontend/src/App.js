b3ba1a9f057b6c325a93ad604b71fa4c42a70ecd
ankita shah ashwin <ankita.shah1301@gmail.com>
2020-02-24 20:54:27 +0100

    Initial react application files

---
+++ frontend/src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Navigation from "../src/components/Navigation";
+import VehicleForm from "../src/components/VehicleForm";
+import Vehicle from "../src/components/Vehicle";
+import HomePage from "../src/pages/HomePage";
+import { Route } from "react-router";
+import { Layout } from "antd"; //Using Ant Design for UI
+
+const { Header, Content, Footer } = Layout;
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Layout className="layout">
+          <Header>
+            <div className="logo" />
+            <Navigation /> {/* Rendering Navigation Component */}
+          </Header>
+          <Content style={{ padding: "0 50px" }}>
+            <div
+              style={{
+                background: "#fff",
+                padding: 24,
+                minHeight: "100em"
+              }}
+            >
+              <Route exact path="/" component={HomePage} />{" "}
+              {/* Rendering HomePage Component */}
+              <Route exact path="/sellVehicle" component={VehicleForm} />{" "}
+              {/* Rendering VehicleForm Component */}
+              <Route
+                exact
+                name="vehicleView"
+                path="/vehicle/:Id"
+                component={Vehicle}
+              />{" "}
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>Footer text here</Footer>
+        </Layout>
+      </div>
+    );
+  }
+}
