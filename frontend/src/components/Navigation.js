07fd1eb787eff5b671ad8df158b102666c5f4d3a
ankita shah ashwin <ankita.shah1301@gmail.com>
2020-02-24 20:54:49 +0100

    Navbar component

---
+++ frontend/src/components/Navigation.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "../styles.css";
+import "antd/dist/antd.css";
+import { Menu } from "antd";
+import { NavLink } from "react-router-dom";
+
+export default class Navigation extends React.Component {
+  render() {
+    return (
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        style={{ lineHeight: "64px" }}
+        selectedKeys={[]}
+      >
+        <Menu.Item key="1">
+          <NavLink to={`/`} />
+          HOME
+        </Menu.Item>
+        <Menu.Item key="2">
+          <NavLink to={`/sellVehicle`} />
+          SELL YOU VEHICLE
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
